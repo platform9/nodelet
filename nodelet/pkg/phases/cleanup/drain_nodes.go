b999df52f82f6361682551f364fb635a432c9b09
srm_6867 <shubham@platform9.com>
2022-02-10 11:05:35 +0530

    changes in kubeutils.go and drain_nodes.go

--- nodelet/pkg/phases/cleanup/drain_nodes.go
+++ nodelet/pkg/phases/cleanup/drain_nodes.go
@@ -2,9 +2,14 @@ package cleanup
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/platform9/nodelet/pkg/utils/config"
 	"github.com/platform9/nodelet/pkg/utils/constants"
+	"github.com/platform9/nodelet/pkg/utils/kubeutils"
+
 	sunpikev1alpha1 "github.com/platform9/pf9-qbert/sunpike/apiserver/pkg/apis/sunpike/v1alpha1"
 )
 
@@ -21,10 +26,30 @@ func (d *DrainNodePhasev2) Status(context.Context, config.Config) error {
 }
 
 func (d *DrainNodePhasev2) Start(context.Context, config.Config) error {
+
+	err := os.Remove(constants.KubeStackStartFileMarker)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (d *DrainNodePhasev2) Stop(context.Context, config.Config) error {
+
+	//TODO : ensure_http_proxy_configured
+
+	if kubeutils.Kubernetes_api_available() {
+		node_identifier := os.Getenv("NODE_NAME")
+		if os.Getenv("CLOUD_PROVIDER_TYPE") == "local" && os.Getenv("USE_HOSTNAME") == "true" {
+			node_identifier = os.Getenv("HOSTNAME")
+		}
+		err := kubeutils.Drain_node_from_apiserver(node_identifier)
+		if err != nil {
+			fmt.Println("Warning: failed to drain node")
+			log.Println(err)
+		}
+	}
+
 	return nil
 }
 
