20572d7241f1b1056fcb5bf4feda3e181715738f
Pushkar Acharya <pacharya@platform9.com>
2022-03-11 22:18:57 +0000

    Fix mock generation and make sure unit tests can be run

--- nodelet/mocks/doc.go
+++ nodelet/mocks/doc.go
@@ -1,3 +1,8 @@
+//go:generate go run -mod=mod github.com/golang/mock/mockgen -package mocks -destination=./mock_phases.go -source=../pkg/phases/phase_interface.go -build_flags=-mod=mod
+//go:generate go run -mod=mod github.com/golang/mock/mockgen -package mocks -destination=./mock_command.go -source=../pkg/utils/command/command.go -build_flags=-mod=mod
+//go:generate go run -mod=mod github.com/golang/mock/mockgen -package mocks -destination=./mock_extension.go -source=../pkg/utils/extensionfile/extension.go -build_flags=-mod=mod
+//go:generate go run -mod=mod github.com/golang/mock/mockgen -package mocks -destination=./mock_fileio.go -source=../pkg/utils/fileio/fileio.go -build_flags=-mod=mod
+
 package mocks
 
 import _ "github.com/golang/mock/mockgen/model"
