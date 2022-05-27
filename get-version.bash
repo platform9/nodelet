4c7cb7fac720fa0d1c96820ed5bc131b903e6c9c
supreeth90 <supreeth90@gmail.com>
2022-05-27 13:38:58 -0700

    Fixing calls to shell scripts for versioning

--- get-version.bash
+++ get-version.bash
@@ -4,17 +4,13 @@
 # 2. Latest git tag using git describe --tags HEAD, if available
 # 3. Using branch name
 if [[ -n "${TAG_NAME}" ]]; then
-  echo "TAG_NAME env found, so using that TAG_NAME:${TAG_NAME}"
   TAG=$TAG_NAME
 elif [[ $(git describe --tags HEAD > /dev/null 2>&1; echo $?) -eq 0 ]]; then
   TAG=$(git describe --tags HEAD)
   IFS='-'
-  echo "A latest tag found in git:${TAG}"
   read -a strarr <<< "$TAG"
   TAG=${strarr[0]}
-  echo "After replacing TAG:${TAG}"
 else
   TAG=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9]/-/g')
-  echo "Using branch name for tagging:${TAG}"
 fi
 echo $TAG
