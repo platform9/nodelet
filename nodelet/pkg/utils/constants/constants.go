9363eb5c452b6de2883c98a7f3ada1585a8ef211
Shubham Mali <shubham@platform9.com>
2022-08-02 20:28:30 +0000

    remove comment

--- nodelet/pkg/utils/constants/constants.go
+++ nodelet/pkg/utils/constants/constants.go
@@ -184,6 +184,6 @@ var (
 
 	// PhaseBaseDir is the base directory in which all bash-based phase scripts are located
 	PhaseBaseDir = "/opt/pf9/pf9-kube/phases"
-	// LocalDir is the local directory used here for containerd binaries.
+
 	ContainerdBaseDir = "/opt/pf9/pf9-kube/containerd"
 )
