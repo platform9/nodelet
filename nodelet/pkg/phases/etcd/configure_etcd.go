fae6ddbe1fe50e3455b8d89de841864f8cd0b02e
Shubham Mali <shubham@platform9.com>
2022-08-29 13:28:56 +0000

    start with configure etcd phase

--- nodelet/pkg/phases/etcd/configure_etcd.go
+++ nodelet/pkg/phases/etcd/configure_etcd.go
@@ -1,20 +1,65 @@
 package etcd
 
 import (
-	"path"
+	"context"
 
-	bashscript "github.com/platform9/nodelet/nodelet/pkg/phases/bash_script_based_phases"
+	"github.com/platform9/nodelet/nodelet/pkg/utils/config"
 	"github.com/platform9/nodelet/nodelet/pkg/utils/constants"
+	"github.com/platform9/nodelet/nodelet/pkg/utils/phaseutils"
+	"go.uber.org/zap"
+
 	sunpikev1alpha1 "github.com/platform9/pf9-qbert/sunpike/apiserver/pkg/apis/sunpike/v1alpha1"
 )
 
-func NewConfigureEtcdPhase(baseDir string) *bashscript.Phase {
-	configureEtcdPhase := &bashscript.Phase{
-		Filename: path.Join(baseDir, "etcd_configure.sh"),
+type ConfigureEtcdPhase struct {
+	HostPhase *sunpikev1alpha1.HostPhase
+	log       *zap.SugaredLogger
+}
+
+func NewConfigureEtcdPhase() *ConfigureEtcdPhase {
+	log := zap.S()
+	return &ConfigureEtcdPhase{
 		HostPhase: &sunpikev1alpha1.HostPhase{
 			Name:  "Configure etcd",
 			Order: int32(constants.ConfigureEtcdPhaseOrder),
 		},
+		log: log,
 	}
-	return configureEtcdPhase
+
+}
+
+func (d *ConfigureEtcdPhase) GetHostPhase() sunpikev1alpha1.HostPhase {
+	return *d.HostPhase
+}
+
+func (d *ConfigureEtcdPhase) GetPhaseName() string {
+	return d.HostPhase.Name
+}
+
+func (d *ConfigureEtcdPhase) GetOrder() int {
+	return int(d.HostPhase.Order)
+}
+
+func (d *ConfigureEtcdPhase) Status(context.Context, config.Config) error {
+
+	d.log.Infof("Running Status of phase: %s", d.HostPhase.Name)
+
+	phaseutils.SetHostStatus(d.HostPhase, constants.RunningState, "")
+	return nil
+}
+
+func (d *ConfigureEtcdPhase) Start(context.Context, config.Config) error {
+
+	d.log.Infof("Running Start of phase: %s", d.HostPhase.Name)
+
+	phaseutils.SetHostStatus(d.HostPhase, constants.RunningState, "")
+	return nil
+}
+
+func (d *ConfigureEtcdPhase) Stop(ctx context.Context, cfg config.Config) error {
+
+	d.log.Infof("Running Stop of phase: %s", d.HostPhase.Name)
+
+	phaseutils.SetHostStatus(d.HostPhase, constants.StoppedState, "")
+	return nil
 }
