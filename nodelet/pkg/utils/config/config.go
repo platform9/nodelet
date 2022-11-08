883382eb5d7af26f48a5869aacff1f9c8775d4e6
NeelavaChatterjee <neelava@platform9.com>
2022-11-08 22:57:41 +0530

    fix conflict due to repeat variable declaration

--- nodelet/pkg/utils/config/config.go
+++ nodelet/pkg/utils/config/config.go
@@ -95,7 +95,6 @@ type Config struct {
 	GRPCRetryTimeoutSeconds   int     `mapstructure:"GRPC_RETRY_TIMEOUT_SECONDS"`
 	NumCmdOutputLinesToLog    int     `mapstructure:"NUM_CMD_OP_LINES_TO_LOG"`
 	CloudProviderType         string  `mapstructure:"CLOUD_PROVIDER_TYPE"`
-	UseHostname               bool    `mapstructure:"USE_HOSTNAME"`
 	MasterIp                  string  `mapstructure:"MASTER_IP"`
 	K8sApiPort                string  `mapstructure:"K8S_API_PORT"`
 	MasterSchedulable         bool    `mapstructure:"ALLOW_WORKLOADS_ON_MASTER"`
